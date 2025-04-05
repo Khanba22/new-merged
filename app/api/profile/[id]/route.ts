1fb19d7af3731cfa30f48546c5db213d45aeee79
Khanba22 <121488978+Khanba22@users.noreply.github.com>
2025-04-05 20:40:25 +0530

    modified casestudy

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -26,3 +26,29 @@ export const GET = async (req: Request) => {
         })
     }
 }
+
+
+export const POST = async (req: Request) => {
+    const id = req.url.split("/").pop()
+    try {
+        const user = await User.findById(id).select("-password")
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, {
+                status: 404,
+            })
+        }
+        const profile = await Profile.findOne({ email: user.email })
+        if (!profile) {
+            return NextResponse.json({ error: "Profile not found" }, {
+                status: 404,
+            })
+        }
+        return NextResponse.json(profile, {
+            status: 200,
+        })
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, {
+            status: 500,
+        })
+    }
+}
